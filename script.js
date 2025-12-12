dbac448ea2bd10a2395ae9d1c82d8fe46f8c6070
piouoiq <melargilliere@gmail.com>
2025-12-12 19:33:49 +0100

    feat: gps coordinates // step 2

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// A toi de jouer pour cette partie :-) Happy coding !
+let inputCity = document.getElementById("cityInput");
+let cityOutput = document.getElementById("city");
+let gpsOutput = document.getElementById("gps");
+let temperatureOutput = document.getElementById("temperature");
+let submitButton = document.getElementById("submitBtn");
+
+submitButton.addEventListener("click", () => {
+  let cityName = inputCity.value;
+  cityOutput.textContent = cityName;
+  fetchCoordinates(cityName);
+});
+
+async function fetchCoordinates(cityName) {
+  const url = "https://nominatim.openstreetmap.org/search?";
+  try {
+    param = new URLSearchParams({
+      q: cityName,
+      format: "json",
+      limit: 1,
+    });
+
+    const response = await fetch(url + param.toString());
+    const data = await response.json();
+    console.log(data);
+
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+    gpsOutput.textContent = `Latitude: ${lat}, Longitude: ${lon}`;
+  } catch (error) {
+    console.error("Error fetching coordinates:", error);
+  }
+}
